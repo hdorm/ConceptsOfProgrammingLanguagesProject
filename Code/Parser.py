c35a6ad6f175a5a4872e7489edf7fcfabc6570de
Hunter Dorminey <hdormine@students.kennesaw.edu>
2022-10-23 18:05:47 -0400

    Scanner can now create a list of lexemes and then remove comments. The parser code demonstrates how to use the scanner functions.

--- Code/Parser.py
+++ Code/Parser.py
@@ -1,11 +1,5 @@
-The parser works with the scanner. Its purpose is to recognize each of the relevant statements or symbols and give
-the token that has been set to identify it as well as the actual thing that is being identified.
-Lexemes Tokens
-index   identifier
-=       equal_sign
-2       int_literal
-*       mult_op
-count   identifier
-+       plus_op
-17      int_literal
-;       semicolon
+from Scanner import *
+
+new_list = name_and_split()
+no_comments_list = delete_comments(new_list)
+print(no_comments_list)
